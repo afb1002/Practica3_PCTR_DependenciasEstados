2a8d7e030f5b1db3175055ff43d69bd669ad620a
migeul <maf1005@alu.ubu.es>
2021-04-13 10:23:47 +0200

    Finalizado ActividadSalidaPuerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,32 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	//
-	// TODO
-	//
+public class ActividadSalidaPuerta implements Runnable{
+
+	private String puerta;
+	private IParque parque;
+	
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+	
+	@Override
+	public void run() {
+		//mientras el parque no esté vacio, se podrá salir de el.
+		while(parque.getContadorPT() > 0) {
+			parque.salirDelParque(puerta);
+			try {
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
+	}
 }
